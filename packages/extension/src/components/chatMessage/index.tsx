a58cd3dd899bf0aadb154c7fb80ffcee5860368f
jaswinder.khartri@fetch.ai <jaswinder.khartri@fetch.ai>
2022-08-02 15:24:24 +0530

    chore: Additional Ui added

--- packages/extension/src/components/chatMessage/index.tsx
+++ packages/extension/src/components/chatMessage/index.tsx
@@ -1,29 +1,40 @@
-import React from "react";
 import classnames from "classnames";
+import React from "react";
+import { Container } from "reactstrap";
+import deliveredIcon from "../../public/assets/icon/delivered.png";
 import style from "./style.module.scss";
+
+const formatTime = (timestamp: number) => {
+  const date = new Date(timestamp * 1000);
+
+  const hours = date.getHours();
+  const minutes = "0" + date.getMinutes();
+  return hours + ":" + minutes.substr(-2);
+};
 
 export const ChatMessage = ({
   message,
   isSender,
+  timestamp,
 }: {
   isSender: boolean;
   message: string;
+  timestamp: number;
 }) => {
   return (
     <div className={isSender ? style.senderAlign : ""}>
-      <div
+      <Container
+        fluid
         className={classnames(style.messageBox, {
           [style.senderBox]: isSender,
         })}
       >
-        <span
-          className={classnames(style.messageText, {
-            [style.senderText]: isSender,
-          })}
-        >
-          {message}
-        </span>
-      </div>
+        <div className={style.message}>{message}</div>
+        <div className={style.timestamp}>
+          {formatTime(timestamp)}
+          {isSender && <img src={deliveredIcon} />}
+        </div>
+      </Container>
     </div>
   );
 };
