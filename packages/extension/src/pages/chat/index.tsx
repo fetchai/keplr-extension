fb75b9ed762234bfea0aa5a5eea921a299e270f7
jas2212 <93518923+jas2212@users.noreply.github.com>
2022-10-13 13:53:31 +0530

    Chat input textarea | reduce number of hits to messaging APi | optimize useeffects (#134)

--- packages/extension/src/pages/chat/index.tsx
+++ packages/extension/src/pages/chat/index.tsx
@@ -35,7 +35,7 @@ import { Users } from "./users";
 
 const ChatView = () => {
   const userState = useSelector(userDetails);
-  
+
   const { chainStore, accountStore, queriesStore } = useStore();
   const current = chainStore.current;
   const accountInfo = accountStore.getAccount(current.chainId);
@@ -61,33 +61,56 @@ const ChatView = () => {
       : current.chainId
   );
 
-  const [userChats, setUserChats] = useState<MessageMap>({});
+  const [userChats, setUserChats] = useState<MessageMap | undefined>();
+  const [loadingChats, setLoadingChats] = useState(false);
   const [inputVal, setInputVal] = useState("");
-  const [openDialog,setIsOpendialog] = useState(false);
+  const [openDialog, setIsOpendialog] = useState(false);
   const [initialChats, setInitialChats] = useState<MessageMap>({});
 
   const requester = new InExtensionMessageRequester();
 
   const registerAndSetMessagePubKey = async () => {
+    setLoadingChats(true);
     try {
       const messagingPubKey = await requester.sendMessage(
         BACKGROUND_PORT,
-        new RegisterPublicKey(current.chainId, userState.accessToken, walletAddress)
+        new RegisterPublicKey(
+          current.chainId,
+          userState.accessToken,
+          walletAddress
+        )
       );
-
       store.dispatch(setMessagingPubKey(messagingPubKey));
-      setIsOpendialog(false);
     } catch (e) {
       // Show error toaster
-      console.error("error", e)
-      setIsOpendialog(false);
+      console.error("error", e);
       // Redirect to home
       history.replace("/");
+    } finally {
+      setIsOpendialog(false);
+      setLoadingChats(false);
     }
-  }
+  };
+
+  useEffect(() => {
+    if (
+      userState?.accessToken.length &&
+      userState?.messagingPubKey.length &&
+      walletAddress
+    ) {
+      messageListener();
+      recieveMessages(walletAddress);
+      fetchBlockList();
+    }
+  }, [
+    userState.accessToken.length,
+    userState.messagingPubKey.length,
+    walletAddress,
+  ]);
 
   useEffect(() => {
     const setJWTAndFetchMsgPubKey = async () => {
+      setLoadingChats(true);
       const res = await getJWT(current.chainId, AUTH_SERVER);
       store.dispatch(setAccessToken(res));
 
@@ -95,33 +118,33 @@ const ChatView = () => {
       if (!pubKey) return setIsOpendialog(true);
 
       store.dispatch(setMessagingPubKey(pubKey));
-    };
 
-    setJWTAndFetchMsgPubKey();
+      setLoadingChats(false);
+    };
+    if (
+      (!userState.accessToken.length || !userState.messagingPubKey.length) &&
+      !loadingChats
+    )
+      setJWTAndFetchMsgPubKey();
   }, [
     current.chainId,
+    loadingChats,
     requester,
+    userState.accessToken.length,
+    userState.messagingPubKey.length,
     walletAddress,
   ]);
-
-  useEffect(() => {
-    if (userState?.accessToken.length && userState?.messagingPubKey) {
-      messageListener();
-      recieveMessages(walletAddress);
-      fetchBlockList();
-    }
-  }, [userState.accessToken, userState.messagingPubKey, walletAddress]);
 
   useEffect(() => {
     const userLastMessages: MessageMap = {};
     Object.keys(messages).map((contact: string) => {
       userLastMessages[contact] = messages[contact].lastMessage;
     });
-    if (Object.keys(initialChats).length === 0) {
+    if (Object.keys(initialChats).length !== Object.keys(messages).length) {
       setUserChats(userLastMessages);
       setInitialChats(userLastMessages);
     }
-  }, [messages]);
+  }, [initialChats, messages]);
   const fillUserChats = () => {
     const userLastMessages: any = {};
     Object.keys(messages).map((contact: string) => {
@@ -185,7 +208,7 @@ const ChatView = () => {
       rightRenderer={<SwitchUser />}
     >
       <div className={style.chatContainer}>
-        {(openDialog && userState.accessToken.length > 0) && (
+        {openDialog && userState.accessToken.length > 0 && (
           <div className={style.popupContainer}>
             {/* <img src={chatIcon} /> */}
             <br />
@@ -215,9 +238,7 @@ const ChatView = () => {
                 menu.
               </p>
             </div>
-            <button
-              type="button"
-              onClick={registerAndSetMessagePubKey}>
+            <button type="button" onClick={registerAndSetMessagePubKey}>
               Continue
             </button>
           </div>
@@ -236,11 +257,15 @@ const ChatView = () => {
             <img src={newChatIcon} alt="" />
           </div>
         </div>
-        <Users
-          chainId={current.chainId}
-          userChats={userChats}
-          addresses={addresses}
-        />
+        {loadingChats ? (
+          <div>Fetching Details. Please Wait ...</div>
+        ) : (
+          <Users
+            chainId={current.chainId}
+            userChats={userChats}
+            addresses={addresses}
+          />
+        )}
       </div>
     </HeaderLayout>
   );
