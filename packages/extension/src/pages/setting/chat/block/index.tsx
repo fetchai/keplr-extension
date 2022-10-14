5e8626bb2b1ae49a6e1961c390c6de0d127b1ee7
jaswinder.khartri@fetch.ai <jaswinder.khartri@fetch.ai>
2022-10-14 11:33:26 +0530

    chore: Add unblock user popup in setting and api logic

--- packages/extension/src/pages/setting/chat/block/index.tsx
+++ packages/extension/src/pages/setting/chat/block/index.tsx
@@ -1,66 +1,112 @@
-import React, { FunctionComponent } from "react";
+import { ExtensionKVStore } from "@keplr-wallet/common";
+import { useAddressBookConfig } from "@keplr-wallet/hooks";
 import { observer } from "mobx-react-lite";
-import { HeaderLayout } from "../../../../layouts";
+import React, { FunctionComponent, useState } from "react";
 import { useIntl } from "react-intl";
-// import { useLanguage } from "../../../languages";
+import { useSelector } from "react-redux";
 import { useHistory } from "react-router";
+import { userMessages } from "../../../../chatStore/messages-slice";
+import { HeaderLayout } from "../../../../layouts";
+import { useStore } from "../../../../stores";
+import { formatAddress } from "../../../../utils/format";
+import { NameAddress } from "../../../chat/users";
 import style from "./style.module.scss";
-// import xIcon from "../../../../public/assets/svg/x-icon.svg";
-// import { PageButton } from "../../page-button";
+import { UnblockUserPopup } from "./unblock-user-popup";
 
 export const BlockList: FunctionComponent = observer(() => {
-    // const language = useLanguage();
-    const history = useHistory();
-    const intl = useIntl();
+  // const language = useLanguage();
+  const messages = useSelector(userMessages);
+  const history = useHistory();
+  const intl = useIntl();
+  const { chainStore } = useStore();
 
-    return (
-        <HeaderLayout
-            showChainName={false}
-            canChangeChainInfo={false}
-            alternativeTitle={intl.formatMessage({
-                id: "setting.block",
-            })}
-            onBackButton={() => {
-                history.goBack();
-            }}
-        >
-            <div className={style.chatContainer}>
-                <div className={style.messagesContainer}>
-                    <div className={style.messageContainer}>
-                        <div className={style.initials}>
-                            J
-                        </div>
-                        <div className={style.messageInner}>
-                            <div className={style.name}>Jerry</div>
-                        </div>
-                        <div>
-                            <img src={require("../../../../public/assets/svg/x-icon.svg")} style={{ width: "80%" }} alt="message" />
-                        </div>
-                    </div>
-                    <div className={style.messageContainer}>
-                        <div className={style.initials}>
-                            H
-                        </div>
-                        <div className={style.messageInner}>
-                            <div className={style.name}>Harry</div>
-                        </div>
-                        <div>
-                            <img src={require("../../../../public/assets/svg/x-icon.svg")} style={{ width: "80%" }} alt="message" />
-                        </div>
-                    </div>
-                    <div className={style.messageContainer}>
-                        <div className={style.initials}>
-                            S
-                        </div>
-                        <div className={style.messageInner}>
-                            <div className={style.name}>Someone</div>
-                        </div>
-                        <div>
-                            <img src={require("../../../../public/assets/svg/x-icon.svg")} style={{ width: "80%" }} alt="message" />
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </HeaderLayout>
-    );
+  const addressBookConfig = useAddressBookConfig(
+    new ExtensionKVStore("address-book"),
+    chainStore,
+    chainStore.current.chainId,
+    {
+      setRecipient: (): void => {
+        // noop
+      },
+      setMemo: (): void => {
+        // noop
+      },
+    }
+  );
+
+  const addresses = addressBookConfig.addressBookDatas.map((data) => {
+    return { name: data.name, address: data.address };
+  });
+  return (
+    <HeaderLayout
+      showChainName={false}
+      canChangeChainInfo={false}
+      alternativeTitle={intl.formatMessage({
+        id: "setting.block",
+      })}
+      onBackButton={() => {
+        history.goBack();
+      }}
+    >
+      <BlockAddresses addresses={addresses} messages={messages} />
+    </HeaderLayout>
+  );
 });
+
+const BlockAddresses: React.FC<{
+  messages: any;
+  addresses: NameAddress[];
+}> = ({ messages, addresses }) => {
+  const [confirmAction, setConfirmAction] = useState<boolean>(false);
+  const [userName, setUserName] = useState<string>("");
+
+  return (
+    <div className={style.chatContainer}>
+      {confirmAction && (
+        <UnblockUserPopup
+          setConfirmAction={setConfirmAction}
+          userName={userName}
+        />
+      )}
+      <div className={style.messagesContainer}>
+        {Object.keys(messages).filter((contact) => messages[contact].isBlocked)
+          .length ? (
+          Object.keys(messages)
+            .filter((contact) => messages[contact].isBlocked)
+            .map((contact) => {
+              const contactName =
+                addresses.find((entry) => entry.address === contact)?.name ||
+                formatAddress(contact);
+              return (
+                <div key={contact} className={style.messageContainer}>
+                  <div className={style.initials}>
+                    {contactName.charAt(0).toUpperCase()}
+                  </div>
+                  <div className={style.messageInner}>
+                    <div className={style.name}>{contactName}</div>
+                  </div>
+                  <div>
+                    <img
+                      src={require("../../../../public/assets/svg/x-icon.svg")}
+                      style={{ width: "80%" }}
+                      alt="message"
+                      onClick={() => {
+                        setUserName(contact);
+                        setConfirmAction(true);
+                      }}
+                    />
+                  </div>
+                </div>
+              );
+            })
+        ) : (
+          <div className={style.messageContainer}>
+            <div className={style.messageInner}>
+              <div className={style.name}>No Addresses Blocked</div>
+            </div>
+          </div>
+        )}
+      </div>
+    </div>
+  );
+};
--- packages/extension/src/pages/setting/chat/index.tsx
+++ packages/extension/src/pages/setting/chat/index.tsx
@@ -1,4 +1,4 @@
-import React, { FunctionComponent, useMemo } from "react";
+import React, { FunctionComponent, useEffect, useMemo, useState } from "react";
 import { observer } from "mobx-react-lite";
 import { HeaderLayout } from "../../../layouts";
 import { useIntl } from "react-intl";
@@ -6,12 +6,59 @@ import { useIntl } from "react-intl";
 import { useHistory } from "react-router";
 import style from "./style.module.scss";
 import { PageButton } from "../page-button";
+import { getJWT } from "../../../utils/auth";
+import {
+  setAccessToken,
+  setMessagingPubKey,
+  userDetails,
+} from "../../../chatStore/user-slice";
+import { fetchPublicKey } from "../../../utils/fetch-public-key";
+import { store } from "../../../chatStore";
+import { AUTH_SERVER } from "../../../config/config";
+import { useSelector } from "react-redux";
+import { useStore } from "../../../stores";
+import { userMessages } from "../../../chatStore/messages-slice";
+import { fetchBlockList } from "../../../graphQL/messages-api";
 
 export const ChatSettings: FunctionComponent = observer(() => {
   // const language = useLanguage();
   const history = useHistory();
   const intl = useIntl();
+  const userState = useSelector(userDetails);
+  const messages = useSelector(userMessages);
+  const { chainStore, accountStore } = useStore();
+  const current = chainStore.current;
+  const walletAddress = accountStore.getAccount(chainStore.current.chainId)
+    .bech32Address;
+  const [loadingChatSettings, setLoadingChatSettings] = useState(false);
+  const [chatPubKeyExists, setChatPubKeyExists] = useState(true);
+  useEffect(() => {
+    const setJWTAndFetchMsgPubKey = async () => {
+      setLoadingChatSettings(true);
+      const res = await getJWT(current.chainId, AUTH_SERVER);
+      store.dispatch(setAccessToken(res));
 
+      const pubKey = await fetchPublicKey(res, current.chainId, walletAddress);
+      if (!pubKey) {
+        setChatPubKeyExists(false);
+        return setLoadingChatSettings(false);
+      }
+      store.dispatch(setMessagingPubKey(pubKey));
+      fetchBlockList();
+      setLoadingChatSettings(false);
+    };
+    if (
+      (!userState.accessToken.length || !userState.messagingPubKey.length) &&
+      !loadingChatSettings
+    )
+      setJWTAndFetchMsgPubKey();
+  }, [
+    current.chainId,
+    loadingChatSettings,
+    userState.accessToken.length,
+    userState.messagingPubKey.length,
+    walletAddress,
+  ]);
   return (
     <HeaderLayout
       showChainName={false}
@@ -28,13 +75,22 @@ export const ChatSettings: FunctionComponent = observer(() => {
           title={intl.formatMessage({
             id: "setting.block",
           })}
-          paragraph={intl.formatMessage({
-            id: "setting.block.paragraph",
-          })}
+          paragraph={
+            chatPubKeyExists
+              ? `${
+                  Object.values(messages).filter((user: any) => user.isBlocked)
+                    .length
+                } Addresses Blocked`
+              : "Please Activate Chat Functionality to Proceed"
+          }
           onClick={() => {
-            history.push({
-              pathname: "/setting/chat/block",
-            });
+            if (
+              Object.values(messages).filter((user: any) => user.isBlocked)
+                .length
+            )
+              history.push({
+                pathname: "/setting/chat/block",
+              });
           }}
           icons={useMemo(
             () => [<i key="next" className="fas fa-chevron-right" />],
