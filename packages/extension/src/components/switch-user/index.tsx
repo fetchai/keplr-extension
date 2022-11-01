5e7f97822b3540e5e2a9102697f96c358d4ee4a3
jaswinder.khartri@fetch.ai <jaswinder.khartri@fetch.ai>
2022-11-01 19:28:15 +0530

    WIP

--- packages/extension/src/components/switch-user/index.tsx
+++ packages/extension/src/components/switch-user/index.tsx
@@ -1,9 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 import { FunctionComponent } from "react";
 import { useHistory } from "react-router";
+import { NotificationPopup } from "./notification-popup";
+import style from "./style.module.scss";
 
 export const SwitchUser: FunctionComponent = () => {
   const history = useHistory();
+  const [notification, setNotification] = useState(false)
+  const [isUnread, setIsUnread] = useState(true)
+  const handleClick = () => {
+    setNotification(!notification)
+    setIsUnread(false)
+  }
 
   return (
     <div
@@ -16,7 +24,7 @@ export const SwitchUser: FunctionComponent = () => {
       }}
     >
       <div
-        style={{ width: "16px", cursor: "pointer" }}
+        style={{ cursor: "pointer", display: "flex", flexDirection: "row" }}
         onClick={(e) => {
           e.preventDefault();
 
@@ -25,6 +33,15 @@ export const SwitchUser: FunctionComponent = () => {
       >
         <i className="fa fa-user" aria-hidden="true" />
       </div>
+      <div onClick={handleClick}>
+        <div className={isUnread ? style.unread : ""}></div>
+        <img
+          src={require("../../public/assets/svg/bell.svg")}
+          style={{ width: "20px", marginLeft: "10px", cursor: "pointer" }}
+          alt="bell"
+        />
+      </div>
+      {notification && <NotificationPopup />}
     </div>
   );
 };
---
+++ packages/extension/src/components/switch-user/notification-popup.tsx
@@ -0,0 +1,85 @@
+import React from "react";
+import style from "./style.module.scss";
+
+export const NotificationPopup = () => {
+    return (
+        <div className={style.popup}>
+            <div className={style.chatContainer}>
+                <div className={style.messagesContainer}>
+                    <div className={style.messageContainer}>
+                        <div className={style.initials}>
+                            A
+                        </div>
+                        <div className={style.messageInner}>
+                            <div className={style.name}>Author</div>
+                            <div className={style.messageText}>Something happened which means something else</div>
+                        </div>
+                    </div>
+                    <div className={style.messageContainer}>
+                        <div className={style.initials}>
+                            A
+                        </div>
+                        <div className={style.messageInner}>
+                            <div className={style.name}>Author</div>
+                            <div className={style.messageText}>Something happened which means something else</div>
+                        </div>
+                    </div>
+                    <div className={style.messageContainer}>
+                        <div className={style.initials}>
+                            A
+                        </div>
+                        <div className={style.messageInner}>
+                            <div className={style.name}>Author</div>
+                            <div className={style.messageText}>Something happened which means something else</div>
+                        </div>
+                    </div>
+                    <div className={style.messageContainer}>
+                        <div className={style.initials}>
+                            A
+                        </div>
+                        <div className={style.messageInner}>
+                            <div className={style.name}>Author</div>
+                            <div className={style.messageText}>Something happened which means something else</div>
+                        </div>
+                    </div>
+                    <div className={style.messageContainer}>
+                        <div className={style.initials}>
+                            A
+                        </div>
+                        <div className={style.messageInner}>
+                            <div className={style.name}>Author</div>
+                            <div className={style.messageText}>Something happened which means something else</div>
+                        </div>
+                    </div>
+                    <div className={style.messageContainer}>
+                        <div className={style.initials}>
+                            A
+                        </div>
+                        <div className={style.messageInner}>
+                            <div className={style.name}>Author</div>
+                            <div className={style.messageText}>Something happened which means something else</div>
+                        </div>
+                    </div>
+                    <div className={style.messageContainer}>
+                        <div className={style.initials}>
+                            A
+                        </div>
+                        <div className={style.messageInner}>
+                            <div className={style.name}>Author</div>
+                            <div className={style.messageText}>Something happened which means something else</div>
+                        </div>
+                    </div>
+                    <div className={style.messageContainer}>
+                        <div className={style.initials}>
+                            A
+                        </div>
+                        <div className={style.messageInner}>
+                            <div className={style.name}>Author</div>
+                            <div className={style.messageText}>Something happened which means something else</div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
