3b09eb8fde90ed014a077bd60aaa2162db590d09
imvinay84 <vsvinaysingh82@gmail.com>
2022-11-29 13:16:42 +0530

    keys changes

--- packages/extension/src/config.ui.var.ts
+++ packages/extension/src/config.ui.var.ts
@@ -1,8 +1,8 @@
 import { IntlMessages } from "./languages";
 import { RegisterOption } from "@keplr-wallet/hooks";
 
-export const PROD_AMPLITUDE_API_KEY = "3fd2416e2a9b3cd13ed4770582995f89";
-export const DEV_AMPLITUDE_API_KEY = "3fd2416e2a9b3cd13ed4770582995f89";
+export const PROD_AMPLITUDE_API_KEY = "";
+export const DEV_AMPLITUDE_API_KEY = "";
 export const ETHEREUM_ENDPOINT =
   "https://mainnet.infura.io/v3/eeb00e81cdb2410098d5a270eff9b341";
 
