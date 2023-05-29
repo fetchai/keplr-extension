1e47e4ec54d7d8554258923e3b0ab20e3056d954
jaswinder.khartri@fetch.ai <jaswinder.khartri@fetch.ai>
2023-05-29 11:49:17 +0530

    fix: Remove script code from chat if present

--- packages/extension/src/utils/process-hyperlinks.ts
+++ packages/extension/src/utils/process-hyperlinks.ts
@@ -1,23 +1,33 @@
 export const processHyperlinks = (inputText: string) => {
-  const processedText = inputText.replace(
-    /(<a\s+(?:[^>]*?\s+)?href=(['"])(?:[^'"]*?)\2[^>]*?>.*?<\/a>)|((https?:\/\/[^\s]+)|(?<!\w)(www\.[^\s]+))/g,
-    (_match: any, anchorTag: string, _quote: any, url: any, plainUrl: any) => {
-      if (anchorTag) {
-        if (anchorTag.includes("target=")) {
-          return anchorTag;
+  const scriptRegex = /<script\b[^<]*(?:(?!<\/script>)<[^<]*)*<\/script>/gi;
+  const urlRegex = /(<a\s+(?:[^>]*?\s+)?href=(['"])(?:[^'"]*?)\2[^>]*?>.*?<\/a>)|((https?:\/\/[^\s]+)|(?<!\w)(www\.[^\s]+))/g;
+  const processedText = inputText
+    .replace(
+      urlRegex,
+      (
+        _match: any,
+        anchorTag: string,
+        _quote: any,
+        url: any,
+        plainUrl: any
+      ) => {
+        if (anchorTag) {
+          if (anchorTag.includes("target=")) {
+            return anchorTag;
+          } else {
+            return anchorTag.replace("<a", '<a target="_blank"');
+          }
         } else {
-          return anchorTag.replace("<a", '<a target="_blank"');
+          const href = url
+            ? url.startsWith("http")
+              ? url
+              : `https://${url}`
+            : `https://${plainUrl}`;
+          return `<a href="${href}" target="_blank">${url}</a>`;
         }
-      } else {
-        const href = url
-          ? url.startsWith("http")
-            ? url
-            : `https://${url}`
-          : `https://${plainUrl}`;
-        return `<a href="${href}" target="_blank">${url}</a>`;
       }
-    }
-  );
+    )
+    .replace(scriptRegex, "<i>Script Not Allowed</i><br />");
 
   return processedText;
 };
