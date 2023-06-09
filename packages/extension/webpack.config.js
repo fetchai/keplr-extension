d1b79b5d4dbce6d126455a0982054cbf43958a71
imvinay84 <vsvinaysingh82@gmail.com>
2023-06-09 19:16:00 +0530

    chore: google auth wip

--- packages/extension/webpack.config.js
+++ packages/extension/webpack.config.js
@@ -177,6 +177,13 @@ const extensionConfig = () => {
         crypto: require.resolve("crypto-browserify"),
         stream: require.resolve("stream-browserify"),
         path: require.resolve("path-browserify"),
+        http: require.resolve("stream-http"),
+        https: require.resolve("https-browserify"),
+        zlib: require.resolve("browserify-zlib"),
+
+        assert: require.resolve("assert"),
+        os: require.resolve("os-browserify"),
+        url: require.resolve("url"),
       },
     },
     module: {
