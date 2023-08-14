998dd15bef0dd64155a6e3d80001eaeaa7f58761
jaswinder.khartri@fetch.ai <jaswinder.khartri@fetch.ai>
2023-08-14 12:00:57 +0530

    cleanup

--- packages/fetch-extension/src/pages/agent-name-service/register-new/index.tsx
+++ packages/fetch-extension/src/pages/agent-name-service/register-new/index.tsx
@@ -1,17 +1,15 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import style from "./style.module.scss";
 import { HeaderLayout } from "../../../new-layouts";
 import { useStore } from "../../../stores";
 import { useNavigate } from "react-router";
 import searchButton from "@assets/icon/search.png";
 import PublicDomainDropdown from "./public-domains-dropdown";
-import {
-  getAllAgentDomains,
-  registerDomain,
-} from "../../../name-service/ans-api";
+import { registerDomain } from "../../../name-service/ans-api";
 import { useNotification } from "@components/notification";
 import { ANS_CONFIG, ANS_TRNSX_AMOUNT } from "../../../config.ui.var";
 import { observer } from "mobx-react-lite";
+
 export const RegisterAgentDomains = observer(() => {
   const { chainStore, accountStore, queriesStore } = useStore();
   const current = chainStore.current;
@@ -19,11 +17,6 @@ export const RegisterAgentDomains = observer(() => {
   const navigate = useNavigate();
   const [searchValue, setSearchValue] = useState("");
   const [agentAddressSearchValue, setAgentAddressSearchValue] = useState("");
-  const [isDomainUnavailable, setIsDomainUnavailable] = useState(false);
-  const [isAgentAddressUnavailable, _setIsAgentAddressUnavailable] =
-    useState(false);
-  // const [isValidAgent, setIsValidAgent] = useState(false);
-  const [isLoading, setIsLoading] = useState(false);
   const notification = useNotification();
   const {
     queryPublicDomains,
@@ -40,23 +33,19 @@ export const RegisterAgentDomains = observer(() => {
     const currentChainId = current.chainId;
     const contractAddress = ANS_CONFIG[currentChainId].contractAddress;
     const parts = domain.split(".");
-
     let permissionsQueryDomain;
     let statusQueryDomain;
-    console.log(parts);
     if (parts.length === 2) {
-      // Case 1: domain is abc.agent
       permissionsQueryDomain = domain;
       statusQueryDomain = domain;
     } else if (parts.length > 2) {
-      // Case 2 and 3: domain is abc.bcd.agent or abc.bcd.def.agent
       permissionsQueryDomain = parts.slice(parts.length - 2).join(".");
       statusQueryDomain = parts.slice(0, parts.length - 1).join(".");
     } else {
-      console.log(`Invalid domain: ${domain}`);
+      domainAvailablityMessage = `Invalid domain`;
+
       return;
     }
-
     const { permissions } = queryPermissions.getQueryContract(
       contractAddress,
       account.bech32Address,
@@ -69,37 +58,28 @@ export const RegisterAgentDomains = observer(() => {
         statusQueryDomain
       );
       if (isAvailable) {
-        domainAvailablityMessage = `You have permissions and the domain ${domain} is available.`;
+        domainAvailablityMessage = `The domain is available`;
         domainAvailablity = true;
       } else {
-        domainAvailablityMessage = `You have permissions, but the domain ${domain} is not available.`;
+        domainAvailablityMessage = `The domain is not available`;
         domainAvailablity = false;
       }
     } else {
-      domainAvailablityMessage = `You do not own the permissions to register ${domain}.`;
+      domainAvailablityMessage = `You do not own the domain`;
       domainAvailablity = false;
     }
   }
   checkDomainRegistration(searchValue);
 
-  useEffect(() => {
-    setIsLoading(true);
-    getAllAgentDomains().then((domainsArray: any) => {
-      setIsDomainUnavailable(domainsArray.includes(searchValue));
-      setIsLoading(false);
-    });
-  }, [searchValue]);
-
   const handleInputChange = (e: any) => {
     const value = e.target.value;
     setSearchValue(value);
   };
-  const { isValid: isValidAgentAddress } =
+  const { isFetching: isLoading, isValid: isValidAgentAddress } =
     queryVaildateAgentAddress.getQueryContract(
       ANS_CONFIG[current.chainId].validateAgentAddressContract,
       agentAddressSearchValue
     );
-  console.log(isValidAgentAddress, "add");
   const handleAgentAddressInputChange = (e: any) => {
     const value = e.target.value;
     setAgentAddressSearchValue(value);
@@ -114,7 +94,7 @@ export const RegisterAgentDomains = observer(() => {
         ANS_TRNSX_AMOUNT,
         notification
       );
-      navigate("/");
+      navigate("/agent-name-service");
     } catch (err) {
       console.error("Error minting domain:", err);
       notification.push({
@@ -127,7 +107,7 @@ export const RegisterAgentDomains = observer(() => {
           duration: 0.25,
         },
       });
-      navigate("/");
+      navigate("/agent-name-service");
     }
   };
   const handleDomainSelect = (selectedDomain: string) => {
@@ -147,20 +127,22 @@ export const RegisterAgentDomains = observer(() => {
       <div
         className={style["searchContainer"]}
         style={{
-          border: isDomainUnavailable
-            ? "1px solid var(--red-red-400, #D38989)"
-            : "1px solid rgba(255, 255, 255, 0.4)",
+          border:
+            !domainAvailablity &&
+            searchValue !== "" &&
+            searchValue.includes(".")
+              ? "1px solid var(--red-red-400, #D38989)"
+              : "1px solid rgba(255, 255, 255, 0.4)",
         }}
       >
-        {" "}
         {isLoading ? (
           <i
-            style={{ color: "white" }}
+            style={{ color: "white", width: "10px" }}
             className="fas fa-spinner fa-spin ml-2"
           />
         ) : searchValue === "" ? (
           <img src={searchButton} className={style["searchIcon"]} alt="" />
-        ) : isDomainUnavailable || !domainAvailablity ? (
+        ) : !domainAvailablity ? (
           <div className={style["domainTakenIcon"]}>!</div>
         ) : (
           <img
@@ -181,28 +163,35 @@ export const RegisterAgentDomains = observer(() => {
           onSelectDomain={handleDomainSelect}
         />
       </div>
-      {isDomainUnavailable && (
-        <div className={style["domainTakenText"]}>
-          <div className={style["domainTakenIcon2"]}>!</div>This domain is taken
+      {!domainAvailablity && searchValue !== "" && (
+        <div
+          style={{ position: "absolute", top: "283px" }}
+          className={style["domainTakenText"]}
+        >
+          <div className={style["domainTakenIcon2"]}>!</div>
+          {domainAvailablityMessage}
         </div>
       )}
       <div
         className={style["searchContainer"]}
         style={{
-          border: isDomainUnavailable
-            ? "1px solid var(--red-red-400, #D38989)"
-            : "1px solid rgba(255, 255, 255, 0.4)",
+          border:
+            !isValidAgentAddress && agentAddressSearchValue !== ""
+              ? "1px solid var(--red-red-400, #D38989)"
+              : "1px solid rgba(255, 255, 255, 0.4)",
+          position: "absolute",
+          top: "315px",
         }}
       >
         {" "}
         {isLoading ? (
           <i
-            style={{ color: "white" }}
+            style={{ color: "white", width: "10px" }}
             className="fas fa-spinner fa-spin ml-2"
           />
         ) : searchValue === "" ? (
           <img src={searchButton} className={style["searchIcon"]} alt="" />
-        ) : isAgentAddressUnavailable || isValidAgentAddress ? (
+        ) : isValidAgentAddress ? (
           <img
             src={require("@assets/svg/agent-domain-available.svg")}
             className={style["availableIcon"]}
@@ -217,22 +206,21 @@ export const RegisterAgentDomains = observer(() => {
           type="text"
           value={agentAddressSearchValue}
           onChange={handleAgentAddressInputChange}
-          disabled={isDomainUnavailable || searchValue === ""}
+          disabled={!domainAvailablity || searchValue === ""}
         />
       </div>
       {!isValidAgentAddress && agentAddressSearchValue !== "" && (
-        <div className={style["domainTakenText"]}>
+        <div
+          style={{ position: "absolute", top: "387px" }}
+          className={style["domainTakenText"]}
+        >
           <div className={style["domainTakenIcon2"]}>!</div>Agent name does not
           exist
         </div>
       )}
-      {
-        <div style={{ color: "white", fontSize: "smaller" }}>
-          {domainAvailablityMessage}
-        </div>
-      }
       <button
         className={style["registerButton"]}
+        disabled={!domainAvailablity || !isValidAgentAddress}
         onClick={() => {
           handleRegisterClick();
         }}
