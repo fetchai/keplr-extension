0afe38e18a430189d003e23d630d43e00584f193
agent-dominatrix <128386379+agent-dominatrix@users.noreply.github.com>
2023-11-01 20:43:01 +0530

    fix: config

--- packages/fetch-extension/src/config.ui.var.ts
+++ packages/fetch-extension/src/config.ui.var.ts
@@ -19,11 +19,11 @@ export const ADDITIONAL_SIGN_IN_PREPEND: RegisterOption[] | undefined =
 
 export const ADDITIONAL_INTL_MESSAGES: IntlMessages = {};
 
-export const MESSAGING_SERVER = "http://localhost:4000/graphql";
-export const SUBSCRIPTION_SERVER = "ws://localhost:4000/subscription";
-export const AUTH_SERVER = "http://localhost:5500";
+// export const MESSAGING_SERVER = "http://localhost:4000/graphql";
+// export const SUBSCRIPTION_SERVER = "ws://localhost:4000/subscription";
+// export const AUTH_SERVER = "http://localhost:5500";
 
-// export const AUTH_SERVER = "https://auth-attila.sandbox-london-b.fetch-ai.com";
+export const AUTH_SERVER = "https://auth-attila.sandbox-london-b.fetch-ai.com";
 
 export const FNS_TEST_ADDRESS = "fetch1s84mudgmjfjmkef7ludqnwy0fchh3mf4p4rmll";
 
@@ -32,26 +32,24 @@ export const CHAIN_ID_FETCHHUB = "fetchhub-4";
 export const GROUP_PAGE_COUNT = 30;
 export const CHAT_PAGE_COUNT = 30;
 
-// let SUBSCRIPTION_SERVER, MESSAGING_SERVER;
+let SUBSCRIPTION_SERVER, MESSAGING_SERVER;
 export let NOTYPHI_BASE_URL: string;
 
 if (process.env.NODE_ENV === "production") {
-  // SUBSCRIPTION_SERVER = "wss://messaging.fetch-ai.network/subscription";
-  // MESSAGING_SERVER = "https://messaging.fetch-ai.network/graphql";
+  SUBSCRIPTION_SERVER = "wss://messaging.fetch-ai.network/subscription";
+  MESSAGING_SERVER = "https://messaging.fetch-ai.network/graphql";
   NOTYPHI_BASE_URL = "https://api.notyphi.com/v1";
 } else {
-  // SUBSCRIPTION_SERVER =
-  //   "wss://messaging-server.sandbox-london-b.fetch-ai.com/subscription";
-  // MESSAGING_SERVER =
-  //   "https://messaging-server.sandbox-london-b.fetch-ai.com/graphql";
+  SUBSCRIPTION_SERVER =
+    "wss://messaging-server.sandbox-london-b.fetch-ai.com/subscription";
+  MESSAGING_SERVER =
+    "https://messaging-server.sandbox-london-b.fetch-ai.com/graphql";
   NOTYPHI_BASE_URL = "https://api-staging.notyphi.com/v1";
 }
 
 const ACTIVITY_SERVER: { [key: string]: string } = {
   [CHAIN_ID_DORADO]: "https://subquery-dorado.fetch.ai/",
   [CHAIN_ID_FETCHHUB]: "https://subquery.fetch.ai/",
-  [CHAIN_ID_SEPOLIA]:
-    "https://sepolia.infura.io/v3/26895da6e4b84f18a635f2e92da42342",
 };
 
 export const GRAPHQL_URL = {
