8b73f756e6cb05dca62ef3c45f0173b95c3a58fa
jaswinder.khartri@fetch.ai <jaswinder.khartri@fetch.ai>
2024-03-29 16:47:52 +0530

    add ibc pages and fix styles

--- packages/fetch-extension/src/components-v2/form/ibc/chain-selector.tsx
+++ packages/fetch-extension/src/components-v2/form/ibc/chain-selector.tsx
@@ -1,29 +1,22 @@
 import React, { FunctionComponent, useState } from "react";
-import {
-  ButtonDropdown,
-  DropdownItem,
-  DropdownMenu,
-  DropdownToggle,
-  FormGroup,
-  Label,
-} from "reactstrap";
+import { FormGroup } from "reactstrap";
+import { Dropdown } from "@components-v2/dropdown";
 
 import style from "./style.module.scss";
-import { IBCChannelRegistrarModal } from "@components/form";
 import { observer } from "mobx-react-lite";
 import { useStore } from "../../../stores";
 import { IIBCChannelConfig } from "@keplr-wallet/hooks";
 import { FormattedMessage } from "react-intl";
+import { Card } from "@components-v2/card";
 
 export const DestinationChainSelector: FunctionComponent<{
   ibcChannelConfig: IIBCChannelConfig;
-}> = observer(({ ibcChannelConfig }) => {
+  setIsIBCRegisterPageOpen: any;
+}> = observer(({ ibcChannelConfig, setIsIBCRegisterPageOpen }) => {
   const { chainStore, ibcChannelStore } = useStore();
   const ibcChannelInfo = ibcChannelStore.get(chainStore.current.chainId);
 
   const [isSelectorOpen, setIsSelectorOpen] = useState(false);
-
-  const [isIBCRegisterModalOpen, setIsIBCregisterModalOpen] = useState(false);
 
   const [selectorId] = useState(() => {
     const bytes = new Uint8Array(4);
@@ -33,67 +26,70 @@ export const DestinationChainSelector: FunctionComponent<{
 
   return (
     <React.Fragment>
-      <IBCChannelRegistrarModal
-        isOpen={isIBCRegisterModalOpen}
-        closeModal={() => setIsIBCregisterModalOpen(false)}
-        toggle={() => setIsIBCregisterModalOpen((value) => !value)}
-      />
       <FormGroup>
-        <Label for={selectorId} className="form-control-label">
+        <div className={style["label"]}>
           <FormattedMessage id="component.ibc.channel-registrar.chain-selector.label" />
-        </Label>
-        <ButtonDropdown
+        </div>
+        <button
+          type="button"
           id={selectorId}
-          className={style["chainSelector"]}
-          isOpen={isSelectorOpen}
-          toggle={() => setIsSelectorOpen((value) => !value)}
+          className={style["selector"]}
+          onClick={() => setIsSelectorOpen(!isSelectorOpen)}
         >
-          <DropdownToggle caret>
-            {ibcChannelConfig.channel ? (
-              chainStore.getChain(ibcChannelConfig.channel.counterpartyChainId)
-                .chainName
-            ) : (
-              <FormattedMessage id="component.ibc.channel-registrar.chain-selector.placeholder" />
-            )}
-          </DropdownToggle>
-          <DropdownMenu>
-            {ibcChannelInfo.getTransferChannels().map((channel) => {
-              if (!chainStore.hasChain(channel.counterpartyChainId)) {
-                return undefined;
-              }
-
-              const chainInfo = chainStore.getChain(
-                channel.counterpartyChainId
-              );
-
-              if (chainInfo) {
-                return (
-                  <DropdownItem
-                    key={chainInfo.chainId}
-                    onClick={(e) => {
-                      e.preventDefault();
-
-                      ibcChannelConfig.setChannel(channel);
-                    }}
-                  >
-                    {chainInfo.chainName}
-                    <div className={style["channel"]}>{channel.channelId}</div>
-                  </DropdownItem>
-                );
-              }
-            })}
-            <DropdownItem
-              onClick={(e) => {
-                e.preventDefault();
-
-                setIsIBCregisterModalOpen(true);
-              }}
-            >
-              <i className="fas fa-plus-circle my-1 mr-1" />{" "}
+          {ibcChannelConfig.channel ? (
+            chainStore.getChain(ibcChannelConfig.channel.counterpartyChainId)
+              .chainName
+          ) : (
+            <FormattedMessage id="component.ibc.channel-registrar.chain-selector.placeholder" />
+          )}
+          <img src={require("@assets/svg/wireframe/chevron-down.svg")} alt="" />
+        </button>
+        <Dropdown
+          title="Destination Chain"
+          isOpen={isSelectorOpen}
+          setIsOpen={setIsSelectorOpen}
+          closeClicked={() => setIsSelectorOpen(!isSelectorOpen)}
+        >
+          {" "}
+          <Card
+            leftImageStyle={{ backgroundColor: "transparent" }}
+            style={{ height: "69px", background: "rgba(255,255,255,0.1)" }}
+            leftImage={<i className="fas fa-plus-circle my-1 mr-1" />}
+            heading={
               <FormattedMessage id="component.ibc.channel-registrar.chain-selector.button.add" />
-            </DropdownItem>
-          </DropdownMenu>
-        </ButtonDropdown>
+            }
+            onClick={(e: any) => {
+              e.preventDefault();
+              setIsIBCRegisterPageOpen(true);
+              setIsSelectorOpen(false);
+            }}
+          />
+          {ibcChannelInfo.getTransferChannels().map((channel) => {
+            if (!chainStore.hasChain(channel.counterpartyChainId)) {
+              return undefined;
+            }
+
+            const chainInfo = chainStore.getChain(channel.counterpartyChainId);
+
+            if (chainInfo) {
+              return (
+                <Card
+                  style={{ background: "rgba(255,255,255,0.1)" }}
+                  heading={chainInfo.chainName}
+                  key={chainInfo.chainId}
+                  onClick={(e: any) => {
+                    e.preventDefault();
+                    ibcChannelConfig.setChannel(channel);
+                    setIsSelectorOpen(false);
+                  }}
+                  subheading={
+                    <div className={style["channel"]}>{channel.channelId}</div>
+                  }
+                />
+              );
+            }
+          })}
+        </Dropdown>
       </FormGroup>
     </React.Fragment>
   );
