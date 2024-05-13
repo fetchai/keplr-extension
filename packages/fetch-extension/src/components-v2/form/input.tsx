f15b467f21e4fe7d128837bc06b5aa132351d297
Abbas Ali <abbas.lokhandwala@fetch.ai>
2024-05-13 10:31:58 +0530

    fix: form components ui

--- packages/fetch-extension/src/components-v2/form/input.tsx
+++ packages/fetch-extension/src/components-v2/form/input.tsx
@@ -80,7 +80,9 @@ export const Input = forwardRef<
         {append}
       </InputGroup>
       {error ? (
-        <FormFeedback style={{ display: "block" }}>{error}</FormFeedback>
+        <FormFeedback style={{ display: "block", marginTop: "3px" }}>
+          {error}
+        </FormFeedback>
       ) : text ? (
         <FormText>{text}</FormText>
       ) : null}
