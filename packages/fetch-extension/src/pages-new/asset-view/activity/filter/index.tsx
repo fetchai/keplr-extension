52c5621e79c581bdd868bdd51474a4370bfeacd6
Abbas Ali <abbas.lokhandwala@fetch.ai>
2024-07-22 22:33:57 +0530

    chore: asset view activity tab

--- packages/fetch-extension/src/pages-new/asset-view/activity/filter/index.tsx
+++ packages/fetch-extension/src/pages-new/asset-view/activity/filter/index.tsx
@@ -1,75 +1,145 @@
-import React, { useState, useRef } from "react";
+import React, { useRef } from "react";
 import styles from "./style.module.scss";
 import { Dropdown } from "@components-v2/dropdown";
+import { Card } from "@components-v2/card";
+import { ButtonV2 } from "@components-v2/buttons/button";
 
 export const FilterActivities: React.FC<{
   onFilterChange: (filter: string[]) => void;
-  options: any[];
-  selectedFilter: any[];
-}> = ({ onFilterChange, options, selectedFilter }) => {
-  const [isOpen, setIsOpen] = useState(false);
+  options?: any[];
+  selectedFilter?: any[];
+  setIsOpen: React.Dispatch<React.SetStateAction<boolean>>;
+  isOpen: boolean;
+  filterTop?: string;
+}> = ({ setIsOpen, isOpen, filterTop }) => {
   const dropdownRef = useRef<HTMLDivElement>(null);
   const toggleDropdown = () => {
     setIsOpen(!isOpen);
   };
-  const handleCheckboxChange = (value: string) => {
-    const newFilters = selectedFilter;
-    if (newFilters.includes(value)) {
-      onFilterChange(newFilters.filter((item) => item !== value));
-    } else {
-      onFilterChange([...newFilters, value]);
-    }
-  };
-
-  const handleDeselectClicks = () => {
-    if (selectedFilter.length != 0) onFilterChange([]);
-  };
-
-  const handleSelectClicks = () => {
-    const allFilters = options.map((option) => option.value);
-    if (selectedFilter.length != allFilters.length) onFilterChange(allFilters);
-  };
-
   return (
-    <div className={styles["dropdown"]}>
-      <div> Activity </div>
-      <div className={styles["dropdownToggle"]} ref={dropdownRef}>
-        <div className={styles["dropdownHeading"]} onClick={toggleDropdown}>
-          Filter
-          <img
-            src={require("@assets/svg/wireframe/filter.svg")}
-            alt="filter"
-            className={styles["arrowIcon"]}
-          />
+    <div className={styles["dropdown-container"]}>
+      <div
+        style={{
+          top: filterTop ? filterTop : "",
+        }}
+        className={styles["dropdown"]}
+        onClick={toggleDropdown}
+        ref={dropdownRef}
+      >
+        <div className={styles["dropdownToggle"]} ref={dropdownRef}>
+          <div className={styles["dropdownHeading"]}>
+            Filter
+            <img
+              src={require("@assets/svg/wireframe/filter.svg")}
+              alt="filter"
+              className={styles["arrowIcon"]}
+            />
+          </div>
         </div>
       </div>
-      <Dropdown
-        isOpen={isOpen}
-        setIsOpen={setIsOpen}
-        title={"Filter"}
-        closeClicked={() => {
-          setIsOpen(false);
-        }}
-      >
-        <div className={styles["select"]}>
-          <div onClick={handleSelectClicks}>Select all</div>
-          <div onClick={handleDeselectClicks}>Unselect all</div>
-        </div>
-        <div className={styles["dropdownMenu"]}>
-          {options.map((option) => (
-            <label key={option.value} className={styles["dropdownItem"]}>
-              <input
-                type="checkbox"
-                className="mx-2"
-                value={option.value}
-                checked={selectedFilter.includes(option.value)}
-                onChange={() => handleCheckboxChange(option.value)}
-              />
-              {option.label}
-            </label>
-          ))}
-        </div>
-      </Dropdown>
     </div>
+  );
+};
+
+interface FilterDropdownProps {
+  isOpen: boolean;
+  setIsOpen: any;
+  options: any;
+  selectedFilter: any;
+  handleCheckboxChange: any;
+  handleSaveChanges: any;
+  isSelectAll: boolean;
+  handleSelectClicks: any;
+  handleDeselectClicks: any;
+  isSaveChangesButtonDisabled: boolean;
+}
+export const FilterDropdown: React.FC<FilterDropdownProps> = ({
+  isOpen,
+  setIsOpen,
+  options,
+  selectedFilter,
+  handleCheckboxChange,
+  handleSaveChanges,
+  isSelectAll,
+  handleSelectClicks,
+  handleDeselectClicks,
+  isSaveChangesButtonDisabled,
+}) => {
+  return (
+    <Dropdown
+      isOpen={isOpen}
+      setIsOpen={setIsOpen}
+      title={"Filter"}
+      closeClicked={() => {
+        setIsOpen(false);
+      }}
+      styleProp={{ position: "block" }}
+    >
+      <div className={styles["select"]}>
+        {!isSelectAll ? (
+          <div className={styles["selectAll"]} onClick={handleSelectClicks}>
+            Select all
+          </div>
+        ) : (
+          <div className={styles["selectAll"]} onClick={handleDeselectClicks}>
+            Unselect all
+          </div>
+        )}
+      </div>
+      <div className={styles["dropdownMenu"]}>
+        {options.map((option: any) => (
+          <label key={option.value} className={styles["dropdownItem"]}>
+            <Card
+              leftImage={option.icon}
+              leftImageStyle={{
+                backgroundColor: "transparent",
+                height: "18px",
+                width: "18px",
+                borderRadius: 0,
+                padding: "1px",
+              }}
+              style={
+                selectedFilter.includes(option.value)
+                  ? {
+                      width: "333px",
+                      background: "var(--Indigo---Fetch, #5F38FB)",
+                    }
+                  : {
+                      width: "333px",
+                      background: "rgba(255,255,255,0.1)",
+                      cursor: "pointer",
+                    }
+              }
+              rightContent={
+                <React.Fragment>
+                  <input
+                    type="checkbox"
+                    className={styles["hidden"]}
+                    value={option.value}
+                    checked={selectedFilter.includes(option.value)}
+                    onChange={() => handleCheckboxChange(option.value)}
+                  />
+                  {selectedFilter.includes(option.value) && (
+                    <img
+                      src={require("@assets/svg/wireframe/filter-check.svg")}
+                      alt=""
+                    />
+                  )}
+                </React.Fragment>
+              }
+              heading={option.label}
+            />
+          </label>
+        ))}
+      </div>
+      <ButtonV2
+        styleProps={{
+          minHeight: "56px",
+        }}
+        disabled={isSaveChangesButtonDisabled}
+        onClick={handleSaveChanges}
+        text="Save Changes"
+      />
+    </Dropdown>
   );
 };
--- packages/fetch-extension/src/pages-new/asset-view/activity/native/index.tsx
+++ packages/fetch-extension/src/pages-new/asset-view/activity/native/index.tsx
@@ -1,35 +1,53 @@
-import { fetchTransactions } from "@graphQL/activity-api";
+import { observer } from "mobx-react-lite";
+import moment from "moment";
 import React, { useEffect, useState } from "react";
+import {
+  CHAIN_ID_DORADO,
+  CHAIN_ID_ERIDANUS,
+  CHAIN_ID_FETCHHUB,
+} from "../../../../config.ui.var";
 import { useStore } from "../../../../stores";
-import { FilterActivities } from "../filter";
-import { ActivityRow } from "./activity-row";
+import { FilterActivities, FilterDropdown } from "../filter";
 import style from "../style.module.scss";
-import { ButtonV2 } from "@components-v2/buttons/button";
+import { ActivityRow } from "./activity-row";
+import styles from "./style.module.scss";
+import { NoActivity } from "../../../activity/no-activity";
+import { UnsupportedNetwork } from "../../../activity/unsupported-network";
 
 const options = [
-  { value: "/cosmos.bank.v1beta1.MsgSend", label: "Funds transfers" },
   {
+    icon: require("@assets/svg/wireframe/arrow-down.svg"),
+    value: "/cosmos.bank.v1beta1.MsgSend",
+    label: "Funds transfers",
+  },
+  {
+    icon: require("@assets/svg/wireframe/stake.svg"),
     value: "/cosmos.staking.v1beta1.MsgDelegate",
     label: "Staked Funds",
   },
   {
+    icon: require("@assets/svg/wireframe/hand-holding-seedling.svg"),
     value: "/cosmos.staking.v1beta1.MsgUndelegate",
     label: "Unstaked Funds",
   },
   {
+    icon: require("@assets/svg/wireframe/rename.svg"),
     value: "/cosmos.staking.v1beta1.MsgBeginRedelegate",
     label: "Redelegate Funds",
   },
   {
+    icon: require("@assets/svg/wireframe/contract-integration-transparent.svg"),
     value:
       "/cosmos.authz.v1beta1.MsgExec,/cosmwasm.wasm.v1.MsgExecuteContract,/cosmos.authz.v1beta1.MsgRevoke",
     label: "Contract Interactions",
   },
   {
+    icon: require("@assets/svg/wireframe/gem.svg"),
     value: "/cosmos.distribution.v1beta1.MsgWithdrawDelegatorReward",
     label: "Claim Rewards",
   },
   {
+    icon: require("@assets/svg/wireframe/arrow-down-up-across-line.svg"),
     value: "/ibc.applications.transfer.v1.MsgTransfer",
     label: "IBC transfers",
   },
@@ -43,116 +61,206 @@ const processFilters = (filters: string[]) => {
   return result;
 };
 
-function debounce(func: any, timeout = 500) {
-  let timer: any;
-  return (...args: any) => {
-    clearTimeout(timer);
-    timer = setTimeout(() => {
-      func(args);
-    }, timeout);
-  };
-}
-
-export const NativeTab = () => {
-  const { chainStore, accountStore } = useStore();
+export const NativeTab = observer(({ filterTop }: { filterTop?: string }) => {
+  const { chainStore, accountStore, analyticsStore, activityStore } =
+    useStore();
   const current = chainStore.current;
   const accountInfo = accountStore.getAccount(current.chainId);
-
-  const [isLoading, setIsLoading] = useState(true);
-  const [loadingRequest, setLoadingRequest] = useState(true);
-  const [fetchedData, setFetchedData] = useState<any>();
-  const [nodes, setNodes] = useState<any>({});
-  const [pageInfo, setPageInfo] = useState<any>();
+  const [isOpen, setIsOpen] = useState(false);
+  const [_date, setDate] = useState("");
+  const [activities, setActivities] = useState<unknown[]>([]);
+  const [isSelectAll, setIsSelectAll] = useState(true);
+  const [isSaveChangesButtonDisabled, setIsSaveChangesButtonDisabled] =
+    useState(true);
   const [filter, setFilter] = useState<string[]>(
     options.map((option) => option.value)
   );
 
-  const fetchNodes = debounce(async (cursor: any) => {
-    setIsLoading(true);
-    const data = await fetchTransactions(
-      current.chainId,
-      cursor,
-      accountInfo.bech32Address,
-      processFilters(filter)
-    );
-    setFetchedData(data?.nodes);
-    if (!pageInfo || cursor != "") setPageInfo(data.pageInfo);
-  }, 1000);
+  // const [isError, setIsError] = useState(false);
+
+  const accountOrChainChanged =
+    activityStore.getAddress !== accountInfo.bech32Address ||
+    activityStore.getChainId !== current.chainId;
 
   useEffect(() => {
-    fetchNodes("");
-  }, [filter]);
+    // this is required because accountInit sets the nodes on reload, so we wait for accountInit to set the node and then setActivities
+    // else activityStore.getNodes will be empty
+    const timeout = setTimeout(() => {
+      setActivities(activityStore.sortedNodes);
+    }, 100);
+
+    return () => {
+      clearTimeout(timeout);
+    };
+  }, []);
 
   useEffect(() => {
-    if (fetchedData) {
-      const nodeMap: any = {};
-      fetchedData.map((node: any) => {
-        nodeMap[node.id] = node;
-      });
-      setNodes({ ...nodes, ...nodeMap });
-      setIsLoading(false);
-      setLoadingRequest(false);
+    if (activityStore.checkIsNodeUpdated === true) {
+      setActivities(activityStore.sortedNodes);
+      activityStore.setIsNodeUpdated(false);
     }
-  }, [fetchedData]);
+  }, [activityStore.checkIsNodeUpdated]);
 
-  const handleClick = () => {
-    setLoadingRequest(true);
-    fetchNodes(pageInfo.endCursor);
-  };
+  useEffect(() => {
+    if (accountOrChainChanged) {
+      activityStore.setAddress(accountInfo.bech32Address);
+      activityStore.setChainId(current.chainId);
+    }
+
+    //accountInit is required because in case of a reload, this.nodes becomes empty and should be updated with KVstore's saved nodes
+    if (accountInfo.bech32Address !== "") {
+      activityStore.accountInit();
+    }
+  }, [
+    accountInfo.bech32Address,
+    current.chainId,
+    accountOrChainChanged,
+    activityStore,
+  ]);
+
+  // const handleClick = () => {
+  //   analyticsStore.logEvent("activity_transactions_click", {
+  //     pageName: "Transaction Tab",
+  //   });
+  //   setLoadingRequest(true);
+  //   fetchNodes(activityStore.getPageInfo.endCursor, "");
+  // };
 
   const handleFilterChange = (selectedFilter: string[]) => {
-    setPageInfo(undefined);
-    setNodes({});
     setFilter(selectedFilter);
+    analyticsStore.logEvent("activity_filter_click", {
+      pageName: "Transaction Tab",
+    });
+  };
+
+  const handleCheckboxChange = (value: string) => {
+    const newFilters = filter.slice();
+    if (newFilters.includes(value)) {
+      setIsSelectAll(false);
+      setFilter(newFilters.filter((item) => item !== value));
+    } else {
+      setFilter([...newFilters, value]);
+      setIsSelectAll(filter.length === options.length);
+    }
+    setIsSaveChangesButtonDisabled(false);
+  };
+
+  const handleDeselectClicks = () => {
+    setIsSelectAll(false);
+    setFilter([]);
+    setIsSaveChangesButtonDisabled(false);
+  };
+
+  const handleSelectClicks = () => {
+    setIsSelectAll(true);
+    setFilter(options.map((option) => option.value));
+    setIsSaveChangesButtonDisabled(false);
+  };
+
+  const handleSaveChanges = () => {
+    setIsSaveChangesButtonDisabled(true);
+    handleFilterChange(filter);
+    setIsOpen(false);
+  };
+  const renderNodes = (nodes: any) => {
+    const renderedNodes: JSX.Element[] = [];
+    Object.values(nodes).forEach(async (node: any, index) => {
+      const currentDate = moment(node.block.timestamp).format(
+        "ddd, DD MMM YYYY"
+      );
+      const previousNode: any =
+        index > 0 ? Object.values(nodes)[index - 1] : null;
+      const previousDate = previousNode
+        ? moment(previousNode.block.timestamp).format("ddd, DD MMM YYYY")
+        : null;
+      const shouldDisplayDate = currentDate !== previousDate;
+      renderedNodes.push(
+        <React.Fragment key={index}>
+          {!shouldDisplayDate && <div className={styles["hr"]} />}
+          {shouldDisplayDate && (
+            <div
+              className={styles["rowSubtitle"]}
+              style={{ marginTop: "12px" }}
+            >
+              {currentDate}
+            </div>
+          )}
+          <ActivityRow setDate={setDate} node={node} />
+        </React.Fragment>
+      );
+    });
+    return renderedNodes;
   };
 
   return (
-    <div>
-      <div className={style["heading"]}>
+    <React.Fragment>
+      <FilterDropdown
+        isOpen={isOpen}
+        setIsOpen={setIsOpen}
+        options={options}
+        selectedFilter={filter}
+        handleCheckboxChange={handleCheckboxChange}
+        handleSaveChanges={handleSaveChanges}
+        isSelectAll={isSelectAll}
+        handleSelectClicks={handleSelectClicks}
+        handleDeselectClicks={handleDeselectClicks}
+        isSaveChangesButtonDisabled={isSaveChangesButtonDisabled}
+      />
+      <div className={style["filter"]}>
         <FilterActivities
           onFilterChange={handleFilterChange}
           options={options}
           selectedFilter={filter}
+          setIsOpen={setIsOpen}
+          isOpen={isOpen}
+          filterTop={filterTop}
         />
       </div>
-      {Object.values(nodes).filter((node: any) =>
-        processFilters(filter).includes(
-          node.transaction.messages.nodes[0].typeUrl
-        )
-      ).length > 0 ? (
-        <React.Fragment>
-          {Object.values(nodes)
-            .filter((node: any) =>
-              processFilters(filter).includes(
-                node.transaction.messages.nodes[0].typeUrl
-              )
-            )
-            .map((node, index) => (
-              <ActivityRow node={node} key={index} />
-            ))}
-          {pageInfo?.hasNextPage && (
-            <ButtonV2
-              disabled={!pageInfo?.hasNextPage || loadingRequest}
-              onClick={handleClick}
-              text={
-                loadingRequest ? (
-                  <i className="fas fa-spinner fa-spin ml-2" />
-                ) : (
-                  "Load more"
+
+      {current.chainId === CHAIN_ID_FETCHHUB ||
+      current.chainId === CHAIN_ID_DORADO ||
+      current.chainId === CHAIN_ID_ERIDANUS ? (
+        // isError ? (
+        //   <ErrorActivity />
+        // ) :
+        activities.length > 0 &&
+        activities.filter((node: any) =>
+          processFilters(filter).includes(
+            node.transaction.messages.nodes[0].typeUrl
+          )
+        ).length > 0 ? (
+          <React.Fragment>
+            {renderNodes(
+              activities.filter((node: any) =>
+                processFilters(filter).includes(
+                  node.transaction.messages.nodes[0].typeUrl
                 )
-              }
-            />
-          )}
-        </React.Fragment>
-      ) : isLoading && filter.length > 0 ? (
-        <div className={style["activityMessage"]}>
-          Loading Activities <i className="fas fa-spinner fa-spin ml-2" />
-        </div>
+              )
+            )}
+            {/* {activityStore.getPageInfo?.hasNextPage && (
+              <ButtonV2
+                text={
+                  loadingRequest ? (
+                    <i className="fas fa-spinner fa-spin ml-2" />
+                  ) : (
+                    "Load more"
+                  )
+                }
+                disabled={
+                  !activityStore.getPageInfo?.hasNextPage ||
+                  loadingRequest === true
+                }
+                onClick={handleClick}
+                styleProps={{ width: "326px" }}
+              />
+            )} */}
+          </React.Fragment>
+        ) : (
+          <NoActivity />
+        )
       ) : (
-        <div className={style["activityMessage"]}>
-          No activity available right now
-        </div>
+        <UnsupportedNetwork chainID={current.chainName} />
       )}
-    </div>
+    </React.Fragment>
   );
-};
+});
