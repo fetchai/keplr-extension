875164832c417d7bdb622de01d65b44713bbd8b4
Abbas Ali <abbas.lokhandwala@fetch.ai>
2024-08-20 11:50:01 +0530

    fix: redirect to gov proposal activity tab on vote submit

--- packages/fetch-extension/src/pages-new/activity/index.tsx
+++ packages/fetch-extension/src/pages-new/activity/index.tsx
@@ -1,9 +1,10 @@
 import { TabsPanel } from "@components-v2/tabs/tabsPanel-2";
 import { HeaderLayout } from "@layouts-v2/header-layout";
 import { observer } from "mobx-react-lite";
-import React, { FunctionComponent, useState } from "react";
+import React, { FunctionComponent, useState, useEffect } from "react";
 import { FormattedMessage, useIntl } from "react-intl";
 import { useNavigate } from "react-router";
+import { useSearchParams } from "react-router-dom";
 import { useStore } from "../../stores";
 import { GovProposalsTab } from "./gov-proposals";
 import { NativeTab } from "./native";
@@ -11,10 +12,10 @@ import style from "./style.module.scss";
 
 export const ActivityPage: FunctionComponent = observer(() => {
   const navigate = useNavigate();
+  const [searchParams] = useSearchParams();
   const intl = useIntl();
   const [latestBlock, _setLatestBlock] = useState<string>();
-  const { analyticsStore } = useStore();
-  const tab = [
+  const tabs = [
     {
       id: "Transactions",
       component: <NativeTab />,
@@ -24,6 +25,22 @@ export const ActivityPage: FunctionComponent = observer(() => {
       component: <GovProposalsTab latestBlock={latestBlock} />,
     },
   ];
+  const [activeTabId, setActiveTabId] = useState(tabs[0].id);
+  const { analyticsStore } = useStore();
+
+  useEffect(() => {
+    // url /activity?tab=Proposals will open gov .proposals tab
+    // url /activity?tab=Transactions will open transactions tab
+    const tab = searchParams.get("tab");
+    const tabIds = {
+      Proposals: "Gov Proposals",
+      Transactions: "Transactions",
+    };
+
+    if (tab === "Proposals") {
+      setActiveTabId(tabIds[tab]);
+    }
+  }, [searchParams]);
 
   return (
     <HeaderLayout
@@ -44,7 +61,8 @@ export const ActivityPage: FunctionComponent = observer(() => {
         {
           <div style={{ width: "326px" }} className={style["tabContainer"]}>
             <TabsPanel
-              tabs={tab}
+              activeTabId={activeTabId}
+              tabs={tabs}
               tabStyle={{
                 margin: "24px 0px 32px 0px",
               }}
