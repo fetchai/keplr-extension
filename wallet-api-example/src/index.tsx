a7aab9da7554d37e3ffabfbd668f29d4bdd85918
agent-dominatrix <128386379+agent-dominatrix@users.noreply.github.com>
2024-08-27 18:51:45 +0530

    chore: remove keplr logo

--- wallet-api-example/src/index.tsx
+++ wallet-api-example/src/index.tsx
@@ -6,22 +6,9 @@ const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
-  // <React.StrictMode>
+  <React.StrictMode>
     <div className="root-container">
-      <div
-        style={{
-          display: "flex",
-          justifyContent: "center",
-          padding: "16px",
-        }}
-      >
-        <img
-          src="/keplr-logo.png"
-          style={{ maxWidth: "200px" }}
-          alt="keplr-logo"
-        />
-      </div>
       <App />
     </div>
-  // </React.StrictMode>
+  </React.StrictMode>
 );
