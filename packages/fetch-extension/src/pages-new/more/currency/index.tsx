37ad7e3163a62d2a7eafa4e43625a7ba7e15a516
sh-wallet <137136919+sh-wallet@users.noreply.github.com>
2024-10-08 13:30:52 +0530

    chore: Added additional currency and UI updates

--- packages/fetch-extension/src/pages-new/more/currency/index.tsx
+++ packages/fetch-extension/src/pages-new/more/currency/index.tsx
@@ -21,6 +21,11 @@ export const CurrencyPge: FunctionComponent = observer(() => {
     []
   );
 
+  const handleClick = (currency: string) => {
+    language.setFiatCurrency(currency);
+    navigate({ pathname: "/" });
+  };
+
   return (
     <HeaderLayout
       showChainName={false}
@@ -59,41 +64,54 @@ export const CurrencyPge: FunctionComponent = observer(() => {
             language.isFiatCurrencyAutomatic ? selectedIcon : undefined
           }
         />
-        {Object.keys(priceStore.supportedVsCurrencies).map((currency) => {
-          // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-          const fiatCurrency = priceStore.supportedVsCurrencies[currency]!;
-
-          return (
-            <Card
-              style={
-                language.fiatCurrency === fiatCurrency.currency &&
-                !language.isFiatCurrencyAutomatic
-                  ? {
-                      background: "var(--Indigo---Fetch, #5F38FB)",
-                      marginBottom: "5px",
-                    }
-                  : { marginBottom: "5px" }
-              }
-              key={fiatCurrency.currency}
-              heading={`${fiatCurrency.currency.toUpperCase()} (${
-                fiatCurrency.symbol
-              })`}
-              onClick={() => {
-                language.setFiatCurrency(fiatCurrency.currency);
-                navigate({
-                  pathname: "/",
-                });
-              }}
-              rightContent={
-                !language.isFiatCurrencyAutomatic
-                  ? language.fiatCurrency === fiatCurrency.currency
-                    ? selectedIcon
-                    : undefined
-                  : undefined
-              }
-            />
-          );
-        })}
+        <div style={{ display: "flex", flexDirection: "column", gap: "6px" }}>
+          {Object.keys(priceStore.supportedVsCurrencies).map((currency) => {
+            // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+            const fiatCurrency = priceStore.supportedVsCurrencies[currency]!;
+            return (
+              <div
+                key={fiatCurrency.currency}
+                className={style["currencyItem"]}
+                style={{
+                  display: "flex",
+                  color: "white",
+                  padding: "18px",
+                  fontSize: "13px",
+                  borderRadius: "12px",
+                  backdropFilter: "blur(10px)",
+                  flexWrap: "wrap",
+                  alignItems: "center",
+                  justifyContent: "space-between",
+                  background:
+                    language.fiatCurrency === fiatCurrency.currency &&
+                    !language.isFiatCurrencyAutomatic
+                      ? "var(--Indigo---Fetch, #5F38FB)"
+                      : "rgba(255, 255, 255, 0.1)",
+                }}
+                onClick={() => handleClick(fiatCurrency.currency)}
+              >
+                <div className={style["currency"]}>
+                  {fiatCurrency.currency.toUpperCase()}
+                </div>
+                <div
+                  style={{
+                    color: "gray",
+                    margin: "4px",
+                  }}
+                >
+                  {`${fiatCurrency.name}  (${fiatCurrency.symbol})`}
+                </div>
+                <div style={{ marginLeft: "auto" }}>
+                  {!language.isFiatCurrencyAutomatic
+                    ? language.fiatCurrency === fiatCurrency.currency
+                      ? selectedIcon
+                      : undefined
+                    : undefined}
+                </div>
+              </div>
+            );
+          })}
+        </div>
       </div>
     </HeaderLayout>
   );
