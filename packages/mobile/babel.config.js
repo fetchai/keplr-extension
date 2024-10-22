7b82f1454b4009c822d8abddc21b1447f2d730d6
sh-wallet <137136919+sh-wallet@users.noreply.github.com>
2024-10-22 19:06:20 +0530

    chore: env implement

--- packages/mobile/babel.config.js
+++ packages/mobile/babel.config.js
@@ -5,5 +5,16 @@ module.exports = {
     ["@babel/plugin-proposal-decorators", { legacy: true }],
     ["@babel/plugin-proposal-class-properties", { loose: true }],
     "react-native-reanimated/plugin",
+    [
+      "transform-inline-environment-variables",
+      {
+        include: [
+          "PROD_AMPLITUDE_API_KEY",
+          "DEV_AMPLITUDE_API_KEY",
+          "PROD_AUTH_CLIENT_ID",
+          "DEV_AUTH_CLIENT_ID",
+        ],
+      },
+    ],
   ],
 };
