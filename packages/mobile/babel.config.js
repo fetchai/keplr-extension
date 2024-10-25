e991e206658a7d93c1042fc4a235453485789504
sh-wallet <137136919+sh-wallet@users.noreply.github.com>
2024-10-25 11:48:11 +0530

    chore: env setup

--- packages/mobile/babel.config.js
+++ packages/mobile/babel.config.js
@@ -5,5 +5,21 @@ module.exports = {
     ["@babel/plugin-proposal-decorators", { legacy: true }],
     ["@babel/plugin-proposal-class-properties", { loose: true }],
     "react-native-reanimated/plugin",
+    [
+      "transform-inline-environment-variables",
+      {
+        include: [
+          "SENTRY_AUTH_TOKEN",
+          "SENTRY_URL",
+          "SENTRY_ORG",
+          "SENTRY_PROJECT",
+          "SENTRY_DSN",
+          "PROD_AMPLITUDE_API_KEY",
+          "DEV_AMPLITUDE_API_KEY",
+          "PROD_AUTH_CLIENT_ID",
+          "DEV_AUTH_CLIENT_ID",
+        ],
+      },
+    ],
   ],
 };
